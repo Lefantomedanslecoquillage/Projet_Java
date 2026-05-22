8b5baad9c3b7b82dae8856083194af5470f6d7fc
Moi_maître_de_l'univers <ewanbkk.charles@gmail.com>
2026-05-22 16:25:22 +0200

    Implémentation model

--- src/main/java/fr/school/smartenergy/model/ConsumptionRecord.java
+++ src/main/java/fr/school/smartenergy/model/ConsumptionRecord.java
@@ -1,3 +1,48 @@
+package fr.school.smartenergy.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class ConsumptionRecord {
-    
+
+    private int id;
+    private int buildingId;
+    private LocalDate date;
+    private LocalTime time;
+    private EnergyType energyType;
+    private double quantity;
+    private double estimatedCost;
+
+    public ConsumptionRecord() {}
+
+    public ConsumptionRecord(int buildingId, LocalDate date, LocalTime time,
+                             EnergyType energyType, double quantity, double estimatedCost) {
+        this.buildingId = buildingId;
+        this.date = date;
+        this.time = time;
+        this.energyType = energyType;
+        this.quantity = quantity;
+        this.estimatedCost = estimatedCost;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getBuildingId() { return buildingId; }
+    public void setBuildingId(int buildingId) { this.buildingId = buildingId; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public LocalTime getTime() { return time; }
+    public void setTime(LocalTime time) { this.time = time; }
+
+    public EnergyType getEnergyType() { return energyType; }
+    public void setEnergyType(EnergyType energyType) { this.energyType = energyType; }
+
+    public double getQuantity() { return quantity; }
+    public void setQuantity(double quantity) { this.quantity = quantity; }
+
+    public double getEstimatedCost() { return estimatedCost; }
+    public void setEstimatedCost(double estimatedCost) { this.estimatedCost = estimatedCost; }
 }
--- src/main/java/fr/school/smartenergy/model/DashboardStats.java
+++ src/main/java/fr/school/smartenergy/model/DashboardStats.java
@@ -1,3 +1,43 @@
+package fr.school.smartenergy.model;
+
+import java.util.List;
+
 public class DashboardStats {
-    
+
+    private double dailyTotal;
+    private double monthlyTotal;
+    private double yearlyTotal;
+    private double totalCost;
+    private String topBuildingName;
+    private List<Alert> alerts;
+
+    public DashboardStats() {}
+
+    public DashboardStats(double dailyTotal, double monthlyTotal, double yearlyTotal,
+                          double totalCost, String topBuildingName, List<Alert> alerts) {
+        this.dailyTotal = dailyTotal;
+        this.monthlyTotal = monthlyTotal;
+        this.yearlyTotal = yearlyTotal;
+        this.totalCost = totalCost;
+        this.topBuildingName = topBuildingName;
+        this.alerts = alerts;
+    }
+
+    public double getDailyTotal() { return dailyTotal; }
+    public void setDailyTotal(double dailyTotal) { this.dailyTotal = dailyTotal; }
+
+    public double getMonthlyTotal() { return monthlyTotal; }
+    public void setMonthlyTotal(double monthlyTotal) { this.monthlyTotal = monthlyTotal; }
+
+    public double getYearlyTotal() { return yearlyTotal; }
+    public void setYearlyTotal(double yearlyTotal) { this.yearlyTotal = yearlyTotal; }
+
+    public double getTotalCost() { return totalCost; }
+    public void setTotalCost(double totalCost) { this.totalCost = totalCost; }
+
+    public String getTopBuildingName() { return topBuildingName; }
+    public void setTopBuildingName(String topBuildingName) { this.topBuildingName = topBuildingName; }
+
+    public List<Alert> getAlerts() { return alerts; }
+    public void setAlerts(List<Alert> alerts) { this.alerts = alerts; }
 }
