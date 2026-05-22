9a11f0ff29b5ffbb07f2b5a954a3015dddffda13
Moi_maître_de_l'univers <ewanbkk.charles@gmail.com>
2026-05-22 16:20:00 +0200

    Implémentation Exception

--- src/main/java/fr/school/smartenergy/exception/AppException.java
+++ src/main/java/fr/school/smartenergy/exception/AppException.java
@@ -1,3 +1,13 @@
-public class AppException {
-    
+package fr.school.smartenergy.exception;
+
+/** Base exception for the Smart Energy Manager application. */
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/fr/school/smartenergy/exception/ImportException.java
+++ src/main/java/fr/school/smartenergy/exception/ImportException.java
@@ -1,3 +1,13 @@
-public class ImportException {
-    
+package fr.school.smartenergy.exception;
+
+/** Thrown when a CSV import fails. */
+public class ImportException extends AppException {
+
+    public ImportException(String message) {
+        super(message);
+    }
+
+    public ImportException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
